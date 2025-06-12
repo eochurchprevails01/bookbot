58961301a8400f9b7c8fea2888f60a49bf59b318
vikingsr001 <jacobloge@gmail.com>
2025-06-11 19:02:23 -0500

    Add sys.argv for book path, add Moby Dick and Pride and Prejudice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import sys
 from stats import count_words, count_characters, get_sorted_char_counts
 
 def get_book_text(filepath):
@@ -6,12 +7,18 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    try:
+        book_text = get_book_text(book_path)
+    except FileNotFoundError:
+        print(f"Error: File {book_path} not found")
+        sys.exit(1)
     num_words = count_words(book_text)
     char_counts = count_characters(book_text)
     sorted_chars = get_sorted_char_counts(char_counts)
-
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
